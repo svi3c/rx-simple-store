d92f25dfb18b567a47194488fd5fb57c27a0d21b
Sven Reglitzki <sven.reglitzki@flavia-it.de>
2017-05-02 14:09:50 +0200

    Implement store and add tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,16 +1,104 @@
-import { beforeEach, it } from "jasmine-promise-wrapper";
+import { it } from "jasmine-promise-wrapper";
 import { Action, RxStore } from "./index";
+
+import "rxjs/add/operator/delay";
+import "rxjs/add/operator/take";
+import "rxjs/add/operator/toPromise";
+import { Subscription } from "rxjs/Subscription";
 
 describe("RxStore", () => {
 
   let store: RxStore<any>;
+  let subscription: Subscription;
 
-  beforeEach(() => {
-    store = new RxStore<any>((state: any, action: Action) => state, {});
+  afterEach(() => {
+    if (subscription && !subscription.closed) {
+      subscription.unsubscribe();
+    }
   });
 
-  it("should work", () => {
-    expect(store).toBeDefined();
+  it("should expose the current state", () => {
+    store = new RxStore<number>((state, action: Action) => action.payload, 0);
+
+    const val1 = store.state;
+    store.dispatch({ type: "foo", payload: 1 });
+    const val2 = store.state;
+
+    expect(val1).toEqual(0);
+    expect(val2).toEqual(1);
+  });
+
+  describe("one observer", () => {
+
+    it("should be notified with the current state on subscription", async () => {
+      store = new RxStore<number>((state, action: Action) => state, 0);
+
+      const initial = await store.take(1).toPromise();
+
+      expect(initial).toEqual(0);
+    });
+
+    it("should be notified when a new state is returned", async () => {
+      store = new RxStore<number>((state, action: Action) => action.payload, 0);
+
+      store.dispatch({ type: "foo", payload: 1 });
+      const updated = await store.delay(100).take(1).toPromise();
+
+      expect(updated).toEqual(1);
+    });
+
+    it("should not be notified when the same state object is returned", () => {
+      let count = 0;
+      store = new RxStore<{ foo: number }>((state, action: Action) => {
+        state.foo = action.payload;
+        return state;
+      }, { foo: 0 });
+
+      subscription = store.subscribe(() => {
+        count++;
+      });
+
+      store.dispatch({ type: "foo", payload: 1 });
+      store.dispatch({ type: "foo", payload: 2 });
+      store.dispatch({ type: "foo", payload: 3 });
+
+      expect(count).toEqual(1);
+    });
+
+  });
+
+  describe("multiple observers", () => {
+
+    it("should be notified with the current state on subscription", async () => {
+      store = new RxStore<number>((state, action: Action) => state, 0);
+
+      const initial1 = await store.take(1).toPromise();
+      const initial2 = await store.take(1).toPromise();
+
+      expect(initial1).toEqual(0);
+      expect(initial1).toEqual(0);
+    });
+
+    it("should be notified when new states are returned", async () => {
+      const states1: number[] = [];
+      const states2: number[] = [];
+      store = new RxStore<number>((state, action: Action) => action.payload, 0);
+      const sub1 = store.subscribe((state) => {
+        states1.push(state);
+      });
+      const sub2 = store.subscribe((state) => {
+        states2.push(state);
+      });
+      subscription = sub1.add(sub2);
+
+      store.dispatch({ type: "foo", payload: 1 });
+      store.dispatch({ type: "foo", payload: 2 });
+      store.dispatch({ type: "foo", payload: 3 });
+
+      expect(states1).toEqual([0, 1, 2, 3]);
+      expect(states2).toEqual([0, 1, 2, 3]);
+    });
+
   });
 
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { BehaviorSubject } from "rxjs/BehaviorSubject";
 import { Observable } from "rxjs/Observable";
+import { Observer } from "rxjs/Observer";
 import { Subject } from "rxjs/Subject";
 
 import "rxjs/add/operator/distinctUntilChanged";
+import "rxjs/add/operator/publishReplay";
 import "rxjs/add/operator/scan";
+import "rxjs/add/operator/startWith";
 
 export interface Action {
   type: string;
@@ -12,15 +14,29 @@ export interface Action {
 
 export type Reducer<S> = (state: S, action: Action) => S;
 
-export class RxStore<S> {
+export class RxStore<S> extends Observable<S> {
 
-  state$: Observable<S>;
-  private actions$: Subject<Action> = new Subject<Action>();
+  state: S;
+  private actions$: Subject<Action>;
 
-  constructor(private reducer: Reducer<S>, initialState: S) {
-    const stateSubject = new BehaviorSubject<S>(initialState);
-    this.state$ = stateSubject.distinctUntilChanged();
-    this.actions$.scan(reducer, initialState).subscribe(stateSubject);
+  constructor(reducer: Reducer<S>, initialState: S) {
+    const actions$ = new Subject<Action>();
+    const state$ = actions$
+      .scan(reducer, initialState)
+      // Publish initial state
+      .startWith(initialState)
+      // Do not publish in-place mutations
+      .distinctUntilChanged()
+      // Cache the current state
+      .publishReplay(1)
+      .refCount();
+    super((observer: Observer<S>) => {
+      state$.subscribe(observer);
+    });
+    state$.subscribe((state) => {
+      this.state = state;
+    });
+    this.actions$ = actions$;
   }
 
   dispatch(action: Action) {
